d167a9921b8084dad8a74154a83045b946341a2d
THARUNJP <tharunroshini71@gmail.com>
2026-03-11 20:45:46 +0530

    error handler corrected

--- templates/ts/small/src/middleware/errorHandler.ts
+++ templates/ts/small/src/middleware/errorHandler.ts
@@ -1,17 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-
-export class AppError extends Error {
-  statusCode: number;
-  isOperational: boolean;
-
-  constructor(message: string, statusCode = 500) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = true;
-
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
+import AppError from "../lib/errors/appError";
 
 export const errorHandler = (
   err: unknown,
